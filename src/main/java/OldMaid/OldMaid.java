fa6db3fe60e1fc94adf395d049bb120131a6bf37
Dominic <seijidominic0@gmail.com>
2021-04-19 04:26:03 -0400

    done

--- src/main/java/OldMaid/OldMaid.java
+++ src/main/java/OldMaid/OldMaid.java
@@ -28,80 +28,89 @@ public class OldMaid {
 
 	 public static void start() {
 
-     	//Automatically dispose all of possible pairs. (Happens only at the start of the game)
+		//Automatically dispose all of possible pairs. (Happens only at the start of the game)
 		for (int i = 0; i < players.size(); i ++) {
-			players.getCurrentData().getHand().disposePairs();
-			players.next();
+		players.getCurrentData().getHand().disposePairs();
+		players.next();
 		}
 		System.out.println("All the pairs in players' hand are discarded.");
 
-     	Scanner s = new Scanner(System.in);
-     	Player previous = null;
-     	Player current = players.getCurrentData();
-     	Hand previousHand = null;
-     	Hand currentHand = current.getHand();
+		Scanner s = new Scanner(System.in);
+		Player previous = null;
+		Player current = players.getCurrentData();
+		Hand previousHand = null;
+		Hand currentHand = current.getHand();
 
-     	//Turn starts here
-     	do {
-			System.out.println("\n" + current.getName() + "'s turn.\n");
+		//Turn starts here
+		do {
+		System.out.println("\n" + current.getName() + "'s turn.\n");
 
-			//pick card from previous player.
-			//If currentPlayer is the first player, skip this if block
-     		if (previous != null) {
-     			previousHand = previous.getHand();
-     			System.out.println(previous + " Pick one!");
-     			for (int i = 1; i <= previousHand.size(); i++) {
-     				System.out.printf("    %d    ", i);
-				}
-				System.out.println();
+		/*
+		 * pick card from previous player.
+		 * If currentPlayer is the first player, skip this if block
+		*/
+		if (previous != null) {
+			previousHand = previous.getHand();
+			System.out.println(previous + " Pick one!");
+			for (int i = 1; i <= previousHand.size(); i++) {
+				System.out.printf("    %d    ", i);
+			}
+			System.out.println();
 
-				int userInput;
-     			do {
-     				userInput = s.nextInt();
-     				s.nextLine();
-				} while(userInput > previousHand.size());
+			int userInput;
+			do {
+				userInput = s.nextInt();
+				s.nextLine();
+			} while(userInput > previousHand.size());
 
-     			Card picked = previousHand.disposeCard(--userInput);
-				System.out.printf("You picked %s from %s!\n", picked, previous.getName());
-     			current.addToHand(picked);
-     			currentHand = current.getHand();
+			Card picked = previousHand.disposeCard(--userInput);
+			System.out.printf("You picked %s from %s!\n", picked, previous.getName());
+			current.addToHand(picked);
+			currentHand = current.getHand();
+		}
+
+		//
+		String input;
+		Card first;
+		Card second;
+		String[] index;
+		do {
+			if (currentHand.isEmpty()){
+				System.out.printf("%s is safe!", current.getName());
+				break;
+			} else if (currentHand.size() == 1 && players.size() == 1) {
+
 			}
 
-     		//
-			String input;
-			Card first;
-			Card second;
-			String[] index;
-			do {
-				if (currentHand.isEmpty()){
-					System.out.printf("%s is safe!", current.getName());
-					break;
-				} else if (currentHand.size() == 1 && players.size() == 1) {
-
-				}
-
-				System.out.println("The cards currently in your hand.");
-				for (int i = 0; i < currentHand.size(); i++) {
-					System.out.printf("%d. %s\n", i + 1, currentHand.get(i));
-				}
-				System.out.println("Enter the number of the pair of cards separated by a whitespace. " +
-						"Type in \"end\" to end turn");
+			System.out.println("The cards currently in your hand.");
+			for (int i = 0; i < currentHand.size(); i++) {
+				System.out.printf("%d. %s\n", i + 1, currentHand.get(i));
+			}
+			System.out.println("Enter the number of the pair of cards separated by a whitespace. " +
+					"Type in \"end\" to end turn");
 
 
-				input = s.nextLine();
-				if (input.equalsIgnoreCase("end"))
-					break;
-				else if (input.equalsIgnoreCase("sort")) {
-					currentHand.sortCards();
-				} else if (input.equalsIgnoreCase("shuffle")) {
-					currentHand.shuffleCards();
-				}
+			/*
+			* Validate user input.
+			* Possible inputs:
+			* 	1. <First card index> <Second card index>;
+			* 	2. end - End turn.
+			* 	3. sort - Sort cards in hand.
+			*   4. shuffle - shuffle cards in hand.
+			* */
+			input = s.nextLine();
+			if (input.equalsIgnoreCase("end"))
+				break;
+			else if (input.equalsIgnoreCase("sort")) {
+				currentHand.sortCards();
+			} else if (input.equalsIgnoreCase("shuffle")) {
+				currentHand.shuffleCards();
+			}
 
-				try {
-					index = input.split(" ");
-					first = currentHand.get(Integer.parseInt(index[0]) - 1);
-					second = currentHand.get(Integer.parseInt(index[1]) - 1);
-
+			try {
+				index = input.split(" ");
+				first = currentHand.get(Integer.parseInt(index[0]) - 1);
+				second = currentHand.get(Integer.parseInt(index[1]) - 1);
 
 				if (first.compareTo(second) == 0) {
 					System.out.printf("Removing %s and %s from your hand.\n", first, second);
@@ -110,27 +119,31 @@ public class OldMaid {
 				} else {
 					System.out.printf("%s and %s does not have the same value.\n", first, second);
 				}
-				} catch (NumberFormatException e) {
-					System.out.println("Please Enter a valid number separated by a whitespace or enter \"end\" to " +
-							"end turn");
-				}
-			} while(!input.equalsIgnoreCase("end"));
 
-     		//Throw pair of cards away
-
-     		if (current.getHand().isEmpty()) {
-				players.destroyCurrent();
-
-			} else {
-     			previous = current;
-     			players.next();
+			} catch (NumberFormatException e) {
+				System.out.println("Please Enter a valid number separated by a whitespace or enter \"end\" to " +
+						"end turn");
 			}
-			current = players.getCurrentData();
 
-			if (players.size() == 1) {
-				System.out.println("WE'VE GOT A LOSER!!!");
-				break;
-			}
+		} while(!input.equalsIgnoreCase("end"));
+
+		/*
+		* From here, check if:
+		* 	1. ...there is a player that has an empty hand. -> remove player from game.
+		*   2. ...the number of of players is greater than 1. If so, end game.
+		* */
+		if (current.getHand().isEmpty()) {
+			players.destroyCurrent();
+		} else {
+			previous = current;
+			players.next();
+		}
+		current = players.getCurrentData();
+
+		if (players.size() == 1) {
+			System.out.println("WE'VE GOT A LOSER!!!");
+			break;
+		}
 
 		} while (players.size() > 1);
 
